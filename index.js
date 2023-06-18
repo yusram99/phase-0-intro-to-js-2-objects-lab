f191ca00db91b0f6094f76d157ff604646a18f85
Your Name <youremail@example.com>
2023-06-18 16:26:49 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11Broadway",
+    };
+    
+    function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {
+    ...employee,
+    [key]: value,
+    };
+    return updatedEmployee;
+    }
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+    }
+    function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+    }
